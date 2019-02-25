d4ef2abb9827bf7cc40df38d6487a5a661557d0e
mark-marshall <mark.marshallgp@gmail.com>
2019-02-25 13:48:50 +0000

    [STRETCH] Added already saved check to stop duplication of movies on SavedList

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,9 @@ export default class App extends Component {
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    savedList.push(movie);
+    if(!this.state.savedList.includes(movie)) {savedList.push(movie);
     this.setState({ savedList });
+    }
   };
 
   render() {
