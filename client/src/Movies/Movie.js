d2d2d908aeeaa8beeed2d3e36602f02c1546aa4a
mark-marshall <mark.marshallgp@gmail.com>
2019-02-25 12:58:45 +0000

    [MVP] Added Home button linking on SavedList

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,8 +10,6 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
-    console.log(this.state.movie);
     const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
