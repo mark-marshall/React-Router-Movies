5b73e092ca52fe7f60bc6a0b424c142de58eeff0
mark-marshall <mark.marshallgp@gmail.com>
2019-02-25 13:18:38 +0000

    [MVP] Removed text-decoration and color form linked movie cards

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -5,3 +5,54 @@ const MovieCard = props => {
 };
 
 export default MovieCard;
+
+
+/*function MovieDetails({ movie }) {
+  const { title, director, metascore, stars, id } = movie;
+  return (
+    <Link to={`/movies/${id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+    </Link>
+  );
+}
+
+    const { title, director, metascore, stars } = this.state.movie;
+    return (
+      <div className="save-wrapper">
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
+        <div className="save-button">Save</div>
+      </div>
+    );
+  }
+}
+*/
