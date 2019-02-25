30790647a0bada45a9d7e751f8826bdd56a6c5d9
mark-marshall <mark.marshallgp@gmail.com>
2019-02-25 13:25:00 +0000

    [STRETCH] Introduced DRY JSX with MovideCard Component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,17 +1,8 @@
 import React from 'react';
 
-const MovieCard = props => {
-  return;
-};
-
-export default MovieCard;
-
-
-/*function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+export default function MovieCard ({ title, director, metascore, stars }) {
   return (
-    <Link to={`/movies/${id}`}>
-      <div className="movie-card">
+    <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -27,32 +18,5 @@ export default MovieCard;
           </div>
         ))}
       </div>
-    </Link>
-  );
+  )
 }
-
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button">Save</div>
-      </div>
-    );
-  }
-}
-*/
